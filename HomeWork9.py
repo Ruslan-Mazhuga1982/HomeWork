8bb40c164be349f0bb639d901f2abe4d0f7dd425
Ruslan-Mazhuga1982 <167924377+Ruslan-Mazhuga1982@users.noreply.github.com>
2024-05-09 18:55:07 +0300

    Add files via upload

--- HomeWork9.py
+++ HomeWork9.py
@@ -3,22 +3,22 @@
 # list_ = ['one', 'two', 'three']
 # for i in list_:
 #     print(i)
-
-
+#
+#
 # list_ = ['one', 'two', 'three']
 # for i in list_:
 #     if i == 'three':
 #         list_.remove(i)
 #
 # print(list_)
-
+#
 # list_ = ['one', 'two', 'three']
 # for i in range (5):
 #     print(i)
 #
 #
 # #print(list_)
-
+#
 # list_ = ['one', 'two', 'three']
 # for i in range (len(list_)):
 #     list_[i] = ' (: '
@@ -32,13 +32,13 @@
 #
 #
 # #print(list_)
-
+#
 # list_ = ['one', 'two', 'three']
 # for i in range (len(list_)):
 #     list_[i] = ' :( '
 #
 # print(list_)
-
+#
 # list_ = ['one', 'two', 'three']
 # list_2 = [2, 3, 4, 5, 1]
 # sum_ = 0
@@ -52,13 +52,20 @@
 #         for k in range(1,200):
 #             print(f'({i} * {j}) * {k} = {(i * j) * k}')
 #         #print(f'{i} + {j} = {i + j}')
-
 #
-cars_count_ = 0
+#
+# cars_count_ = 0
+# cars_ = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+# for i in cars_:
+#     print('Я езжу на автомобиле марки', i)
+# for j in range(1, 11):
+#     print(f'{i} * {j} * 10 = {i * j}')
+#cars_count_ = 0
 cars_ = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count = 0
 for i in cars_:
-    print('Я езжу на автомабиле марки', i)
-for j in range(1, 11):
-    print(f'{i} * {j} * 10 = {i * j}')
+    print('Я езжу на автомобиле марки', i, cars_count)
+    cars_count += 10
+
 
 
