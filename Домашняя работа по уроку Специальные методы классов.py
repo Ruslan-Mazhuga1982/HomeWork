5e19d9e5ffd3886cad48b200faa7561a81f55b48
Ruslan-Mazhuga1982 <167924377+Ruslan-Mazhuga1982@users.noreply.github.com>
2024-06-10 22:39:29 +0300

    Add files via upload

--- Домашняя работа по уроку Специальные методы классов.py
+++ Домашняя работа по уроку Специальные методы классов.py
@@ -1,14 +1,21 @@
-class house():
-    def __init__(self, numberOfFloors):
-        self.numberOfFloors = numberOfFloors
+# Создайте новый класс House
+# Создайте инициализатор для класса House, который будет задавать атрибут этажности self.numberOfFloors = 0
+# Создайте метод setNewNumberOfFloors(floors), который будет изменять атрибут numberOfFloors на параметр floors и выводить в консоль numberOfFloors
+# Полученный код напишите в ответ к домашему заданию
+class House():
+    # numberOfFloors = 0
+    # floors = 8
 
-    numberOfFloors = 0
+    def __init__(self):
+        self.numberOfFloors = 0
+
 
     def setNewNumberOfFloors(self, floors):
-        self.floors = floors
+        self.numberOfFloors = floors
+        #print(self.numberOfFloors)
 
-    floors = 8
-    for i in range(floors):
-        i = floors
-        numberOfFloors += floors
-        print(numberOfFloors)
+
+h1 = House()
+#print(h1.numberOfFloors)
+h1.setNewNumberOfFloors(15)
+print(h1.numberOfFloors)
